779b1c0bc5f0a8997ba2910a23d99bc62b91798b
cherrera0001 <herrera.jara.cristobal@gmail.com>
2024-08-18 04:14:28 -0400

    Minifi js
    Pase por teser los js para minificarlos

--- assets/js/loadPartials.js
+++ assets/js/loadPartials.js
@@ -1,11 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Cargar navbar
-    fetch('../partials/navbar.html')
-        .then(response => response.text())
-        .then(data => document.getElementById('navbar-placeholder').innerHTML = data);
-
-    // Cargar footer
-    fetch('../partials/footer.html')
-        .then(response => response.text())
-        .then(data => document.getElementById('footer-placeholder').innerHTML = data);
-});
+document.addEventListener("DOMContentLoaded",()=>{fetch("../partials/navbar.html").then(e=>e.text()).then(e=>document.getElementById("navbar-placeholder").innerHTML=e),fetch("../partials/footer.html").then(e=>e.text()).then(e=>document.getElementById("footer-placeholder").innerHTML=e)})
--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,18 +1,1 @@
-// theme.js
-
-document.addEventListener('DOMContentLoaded', () => {
-    const toggleButton = document.getElementById('theme-toggle');
-    const currentTheme = localStorage.getItem('theme') || 'light';
-    
-    // Aplica el tema actual almacenado en localStorage
-    document.body.classList.toggle('dark-mode', currentTheme === 'dark');
-    
-    toggleButton.addEventListener('click', () => {
-        // Cambia el tema
-        const newTheme = document.body.classList.contains('dark-mode') ? 'light' : 'dark';
-        document.body.classList.toggle('dark-mode', newTheme === 'dark');
-        
-        // Guarda el tema en localStorage
-        localStorage.setItem('theme', newTheme);
-    });
-});
+document.addEventListener("DOMContentLoaded",()=>{const e=document.getElementById("theme-toggle");var t=localStorage.getItem("theme")||"light";document.body.classList.toggle("dark-mode","dark"===t),e.addEventListener("click",()=>{var e=document.body.classList.contains("dark-mode")?"light":"dark";document.body.classList.toggle("dark-mode","dark"==e),localStorage.setItem("theme",e)})})
